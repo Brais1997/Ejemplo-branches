a3a6b391a819673a412d3afcb870451ff354954e
galle <galle@DESKTOP-J6RSNG6>
2016-03-13 21:10:33 +0100

    - Ejemplo manu.

---
+++ src/practicando/branches/PracticandoBranches.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicando.branches;
+
+/**
+ *
+ * @author galle
+ */
+public class PracticandoBranches {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Metodos obj = new Metodos();
+        obj.manu();
+    }
+    
+}
